aa60282c0422a3e170c0647e02f2a4fb48b38524
Gable Brown <gbrown57@uncc.edu>
2019-02-14 21:55:08 -0500

    Added some more advanced printing examples

--- 1_print.py
+++ 1_print.py
@@ -1,11 +1,58 @@
 # import time library
 import time
 
-# print a string to the console
-print("I like bananas...")
 
-# wait two seconds
-time.sleep(2)
+# Print a string to output
+print("Hello there!")
 
-# print another string to the console
-print("But raspberries are cool too.")
+# Wait 1 second
+time.sleep(1)
+
+# Define a string
+string = "My name is pi."
+
+# Print predefined string
+print(string)
+
+# Wait 500 milliseconds
+time.sleep(.5)
+
+
+# Save user input to a variable (Aka their name)
+variable = input("What's your name? ")
+
+# Wait 1 second
+time.sleep(1)
+
+
+# Print a string with a variable in the middle
+print("Hello {0}, it's nice to see you.".format(variable))
+
+# Advanced printing!
+# Define a list of fruits
+fruits = ["Apples","Bananas","Oranges","Peaches","Raspberries"]
+
+# Print out the preliminary statement without a line break
+print("I like ", end="")
+
+# For each fruit in fruits, where fruit = fruits[i]
+for i, fruit in enumerate(fruits):
+  # Print which the fruit
+  print(fruit, end="")
+  # If we're not at second to last element
+  if i < len(fruits) - 2:
+    # Print a comma
+    print(", ", end="")
+  # If we're at the second to last element
+  elif i == len(fruits) - 2:
+    # Print "and" with an oxford comma
+    print(", and ", end="")
+  # Else (we're probably at the end of the list)
+  else:
+    # Print a period with a line break
+    print(".")
+
+    # aka 'print(".", end="\n")''
+
+
+
