4092ee5b6d1f49b09b80999abb127364fb94257d
C1ARKGABLE <yellownovacrew@gmail.com>
2019-03-01 17:17:21 -0500

    Fixed indention

--- 2_blink.py
+++ 2_blink.py
@@ -18,21 +18,21 @@ GPIO.setmode(GPIO.BOARD)
 # For each LED in our list of LEDs
 for ledX in leds:
   # Set LEDs mode to output
-	GPIO.setup(ledX, GPIO.OUT)
+  GPIO.setup(ledX, GPIO.OUT)
   # Set led to low(0V) for clean start
-	GPIO.output(ledX, GPIO.LOW)
+  GPIO.output(ledX, GPIO.LOW)
 
 try:
     # Loop forever
     while True:
-  		for ledX in leds:
+        for ledX in leds:
         # Set led to high(3V)
-  			GPIO.output(ledX, GPIO.HIGH)
-  			time.sleep(0.2)
-  		for ledX in leds:
+            GPIO.output(ledX, GPIO.HIGH)
+            time.sleep(0.2)
+        for ledX in leds:
         # Set led to low(0V)
-  			GPIO.output(ledX, GPIO.LOW)
-  			time.sleep(0.2)
+            GPIO.output(ledX, GPIO.LOW)
+            time.sleep(0.2)
 # Exit on ctrl+c
 except KeyboardInterrupt:
     GPIO.cleanup()
